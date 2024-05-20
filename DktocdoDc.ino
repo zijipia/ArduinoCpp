4f6cb516538eed5e91b8cf0e48efec1817fd45ca
Ziji <104454302+zijipia@users.noreply.github.com>
2024-05-20 23:40:08 +0700

    Create DktocdoDc.ino

---
+++ DktocdoDc.ino
@@ -0,0 +1,22 @@
+int TocDo = 50;
+bool dir = 1;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(12,OUTPUT);
+pinMode(13,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+digitalWrite(12,1);
+digitalWrite(13,1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(dir){
+  analogWrite(10,TocDo);
+  digitalWrite(11,0);
+  }else{
+  analogWrite(11,TocDo);
+  digitalWrite(10,0);
+  }
+}
